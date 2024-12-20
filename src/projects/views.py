3b8db05d0170466b2877aad3833ecf4598db1383
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-20 13:04:15 +0300

    Registered Projectto admin and added views

--- src/projects/views.py
+++ src/projects/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+
+def project_index(request: HttpRequest):
+    """
+    renders html page containing all projects info
+    Arguments:
+        request (HttpRequest): request object
+    """
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/project_index.html", context)
+
+
+def project_detail(request: HttpRequest, pk: int):
+    """
+    renders html page for project
+    Arguments:
+        request (HttpRequest): request object
+        pk (int): the primar key - id
+    """
+    project = Project.objects.get(pk=pk)
+    context = {"project": project}
+    return render(request, "projects/project_detail.html", context)
