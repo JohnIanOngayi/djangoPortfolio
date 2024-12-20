e7e51bd06bac07e0ebb2fcc31e772c6297384fd3
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-20 11:59:37 +0300

    Added projects application

--- src/main/settings.py
+++ src/main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # pages application
     "pages.apps.PagesConfig",
+    # projects application
+    "projects.apps.ProjectsConfig",
 ]
 
 MIDDLEWARE = [
