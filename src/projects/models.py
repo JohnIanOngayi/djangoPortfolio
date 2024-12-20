1e70df526591204724d3b00450e5dcdd888f77f6
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-20 13:34:19 +0300

    Done n' Dusted

--- src/projects/models.py
+++ src/projects/models.py
@@ -8,9 +8,11 @@ class Project(models.Model):
     Attributes:
         title (str): project title
         description (text): project description
-        technology(str): project stack
+        technology (str): project stack
+        image (img): project banner
     """
 
     title: models.CharField = models.CharField(max_length=100)
     description: models.TextField = models.TextField()
     technology: models.CharField = models.CharField(max_length=20)
+    image: models.FileField = models.FileField(upload_to="project_images/", blank=True)
