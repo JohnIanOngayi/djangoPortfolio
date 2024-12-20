05dfa6c7dee96d66702c49b0ddbe1d08e0118259
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-20 12:23:42 +0300

    Successfully hooked up projects

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,3 +1,16 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    A class representing a project
+    Attributes:
+        title (str): project title
+        description (text): project description
+        technology(str): project stack
+    """
+
+    title: models.CharField = models.CharField(max_length=100)
+    description: models.TextField = models.TextField()
+    technology: models.CharField = models.CharField(max_length=20)
