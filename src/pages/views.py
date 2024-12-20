54f194d5f3aed66f0887e4f62a38f1203ae1e46f
JohnIanOngayi <ongayijohnian@gmail.com>
2024-12-20 11:40:24 +0300

    Successfully hooked up templates
    - Serve '/' with home.html

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request: HttpRequest):
+    """
+    Renders a html page
+    Args:
+        request (HttpRequest): a django request object
+    Returns:
+        html page render
+    """
+    return render(request, "pages/home.html", {})
